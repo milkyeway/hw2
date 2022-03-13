f3d64931c9aefced7fde6cd878fde3a75fb8087d
milkyeway <namilkywaw@gmail.com>
2022-03-13 20:05:35 +0800

    hw4.js

--- js/homework4.js
+++ js/homework4.js
@@ -1,70 +1,28 @@
-// jQuery.getJSON( url [, data ] [, success ] )
-$.getJSON("/js/public/data-zh.json", "", function (data) {
-  //each迴圈 使用$.each方法遍歷返回的資料date
-  // console.log(data.districts)
-  
+// console.log(data);
+// console.log(data[0].districts);
+
+$.each(data, function (index, value) {
+  // console.log(index);
   // console.log(data);
-  
-  $.each(data, function (index, value) {
-    // console.log(index,'1');
-    // console.log(value,'2');
-    // console.log(value.name,'3');
-    console.log(data,'4');
-    
-    $.each(value.districts, function (index, value) {
-      // console.log(value.districts.length);
-      // console.log(data[0].districts);
-      console.log(districts);
-      // console.log(value.zip);
-      // console.log(value.name);
-    })
+  // console.log(value);
+  // console.log(value.name); //*th
+  // console.log(value.districts);
+  // console.log(value.districts.length);
+
+  let value_add = value.districts.length + 1;
+  // console.log(value_add);
+
+  // let str = '<tr><th class="th" rowspan="' + value.districts.length + '">' + value.name + '</th></tr>';
+  let str = '<tr><th class="th" rowspan="' + value_add + '">' + value.name + '</th></tr>';
+  $('.tbody').append(str);
+
+  $.each(value.districts, function (index, value) {
+    // console.log(value);
+    // console.log(value.zip);
+    // console.log(value.name);
+
+    let str = '<tr><td>' + value.name + '</td><td class="k">' + value.zip + '</td></tr>';
+    $('.tbody').append(str);
+
   })
-
-  // $.each(data.districts, function (j) {
-  //   let c_text = data.districts
-  //   let c = j;
-  //   // console.log(c_text[c])
-  //   // let str1 = '<tr data-id="' + c + '"><th rowspan="' + +'"></th></tr>'
-  //   // $('.thead').append(str1);
-
-
-  //   $.map(c_text[c][0], function (k) {
-  //     // console.log(k)
-  //     let str = '<tr><td>' + k + '</td></tr>'
-  //     $('.tbody').append(str);
-  //   })
-
-  //   $.map(c_text[c][1], function (o) {
-  //     // console.log(o)
-  //     let str = '<tr><td>' + o + '</td></tr>'
-  //     $('.tbody1').append(str);
-  //   })
-  // })
-
-  // $.each(data.counties, function (i) {
-  //   let a = data.districts[i][0].length
-  //   let b = data.counties[i]
-  //   // console.log(a)
-  //   // console.log(b)
-  //   // console.log(b)
-  //   for (e = 0; e < $('thead tr').length; e++) {
-  //     // let en = $('.thead tr:nth-child(' + (e + 1) + ')').data('id');
-  //     // let ae = $('.thead tr').data('id', en)[e]
-  //     // console.log(en)
-  //     // console.log(ae)
-  //     // // console.log(e1)
-  //     // let str0 = '<tr data-id="' + c + '"><th rowspan="' + +'"></th></tr>'
-  //     // console.log(i)
-  //     console.log(e)
-  //     // < tr data-id="' + c + '" > <th rowspan="' + +'"></th></ >
-
-  //     // $('').append(str0)
-  //     // console.log(e) //0
-  //     // console.log($('.thead tr:nth-child(' + (e + 1) + ')').data('id'))
-  //     // console.log(e) //1
-  //     // console.log($('.thead tr:nth-child(' + (e + 1) + ')').data('id'))
-
-  //   }
-
-  // })
-});
+})
